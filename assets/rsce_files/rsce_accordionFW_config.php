a292f84057aefa4cd7b612427d56adcfa9448e4b
Jeff <quentin@webexmachina.fr>
2018-10-29 18:14:43 +0100

    + added rsce testimonials + added rsce accordionFW
    + added themes_framway folder, with smartgear's theme included

---
+++ assets/rsce_files/rsce_accordionFW_config.php
@@ -0,0 +1,71 @@
+<?php
+// rsce_accordionFW_config.php
+return array
+(
+    'label' => array('Accordion FW', 'Générez un accordéon et configurez les éléments')
+    ,'types' => array('content')
+    ,'contentCategory' => 'includes'
+    ,'standardFields' => array('cssID')
+    ,'fields' => array
+    (
+        'config_legend' => array
+        (
+            'label' => array('Configuration de l\'accordéon')
+            ,'inputType' => 'group'
+        )
+        ,'deploy_all' => array
+        (
+            'label' => array("Tout déployer", "Cochez pour déployer automatiquement tous les éléments")
+            ,'inputType' => 'checkbox'
+            ,'eval' => array('tl_class'=>'w50 clr')
+        )
+        ,'disable_collapse' => array
+        (
+            'label' => array("Désactiver déployer/replier", "Cochez pour désactiver les actions déployer/replier (déploie automatiquement tous les éléments)")
+            ,'inputType' => 'checkbox'
+            ,'eval' => array('tl_class'=>'w50')
+        )
+        ,'auto_collapse' => array
+        (
+            'label' => array("Auto repliage", "Cochez pour replier automatiquement les éléments lors du déploiement de l'un d'eux")
+            ,'inputType' => 'checkbox'
+            ,'eval' => array('tl_class'=>'w50')
+        )
+
+        // Items
+        ,'items' => array
+        (
+            'label' => array('Eléments', 'Editez les éléments')
+            ,'elementLabel' => '%s. élément'
+            ,'inputType' => 'list'
+            ,'fields' => array
+            (
+                // Content
+                'title' => array
+                (
+                    'label' => &$GLOBALS['TL_LANG']['tl_content']['headline']
+                    ,'inputType' => 'text'
+                    ,'eval' => array('tl_class'=>'w50','mandatory'=>true)
+                )
+                ,'content' => array
+                (
+                    'label' => array('Contenu', 'Saisissez le contenu textuel de l\'élément')
+                    ,'inputType' => 'textarea'
+                    ,'eval' => array('rte' => 'tinyMCE', 'tl_class' => 'clr','mandatory'=>true)
+                )
+                ,'lock' => array
+                (
+                    'label' => array("Lock", "Cochez pour que l'élément soit toujours visible (désactive les actions déployer/replier)")
+                    ,'inputType' => 'checkbox'
+                    ,'eval' => array('tl_class'=>'w50')
+                )
+                ,'active' => array
+                (
+                    'label' => array("Active", "Cochez pour que l'élément soit déployé automatiquement au chargement de la page")
+                    ,'inputType' => 'checkbox'
+                    ,'eval' => array('tl_class'=>'w50')
+                )
+            )
+        )
+    )
+);
---
+++ assets/rsce_files/rsce_testimonials_config.php
@@ -0,0 +1,106 @@
+<?php
+// rsce_testimonials_config.php
+return array
+(
+    'label' => array('Testimonials', 'Générez un slider affichant des témoignages/citations')
+    ,'types' => array('content')
+    ,'contentCategory' => 'includes'
+    ,'standardFields' => array('cssID')
+    ,'fields' => array
+    (
+        'config_legend' => array
+        (
+            'label' => array('Configuration du slider')
+            ,'inputType' => 'group'
+        )
+        ,'slide_height' => array
+        (
+            'label' => array('Hauteur du slider', 'Configurez la hauteur du slider')
+            ,'inputType' => 'text'
+            ,'eval' => array('tl_class' => 'w50')
+        )
+        ,'slide_autoplay' => array
+        (
+            'label' => array("Démarrage automatique", "Cochez pour que les contenus défilent automatiquement")
+            ,'inputType' => 'checkbox'
+            ,'eval' => array('tl_class'=>'w50 clr')
+        )
+        ,'slider_loop' => array
+        (
+            'label' => array("Loop", "Cochez pour visualiser les contenus en boucle")
+            ,'inputType' => 'checkbox'
+            ,'eval' => array('tl_class'=>'w50')
+        )
+
+        ,'config_nav_legend' => array
+        (
+            'label' => array('Configuration de la navigation')
+            ,'inputType' => 'group',
+        )
+        ,'nav_display' => array
+        (
+            'label' => array('Affichage de la navigation', 'Si souhaité, ajustez la position des boutons de navigation du slider')
+            ,'inputType' => 'select'
+            ,'options' => array(''=>'Après', 'inner'=>'A l\'intérieur', 'hidden'=>'Caché')
+            ,'eval' => array('tl_class'=>'w50')
+        )
+        ,'nav_horizontal' => array
+        (
+            'label' => array('Position horizontale', 'Si souhaité, ajustez la position horizontale du contenu')
+            ,'inputType' => 'select'
+            ,'options' => array(''=>'Aucun', 'left' => 'A gauche', 'right' => 'A droite')
+            ,'eval' => array('tl_class'=>'w50')
+        )
+        ,'nav_vertical' => array
+        (
+            'label' => array('Position verticale', 'Si souhaité, ajustez la position verticale du contenu')
+            ,'inputType' => 'select'
+            ,'options' => array(''=>'Aucun', 'top'=>'En Haut', 'bottom'=>'En Bas')
+            ,'eval' => array('tl_class'=>'w50')
+        )
+        ,'nav_arrows' => array
+        (
+            'label' => array("Navigation fléchée", "Cochez pour remplacer la navigation par des flèches latérales")
+            ,'inputType' => 'checkbox'
+            ,'eval' => array('tl_class'=>'w50 clr')
+        )
+
+        // Items
+        ,'items' => array
+        (
+            'label' => array('Témoignages/citations', 'Editez les témoignages/citations')
+            ,'elementLabel' => '%s. témoignage/citation'
+            ,'inputType' => 'list'
+            ,'fields' => array
+            (
+                // Background
+                'slide_img_src' => array
+                (
+                    'label' => array('Fond', 'Insérez une image qui sera utilisé comme fond de cet élément')
+                    ,'inputType' => 'fileTree'
+                    ,'eval' => array('filesOnly'=>true, 'fieldType'=>'radio', 'extensions'=>Config::get('validImageTypes'))
+                )
+                ,'slide_img_alt' => array
+                (
+                    'label' => &$GLOBALS['TL_LANG']['tl_content']['alt']
+                    ,'inputType' => 'text'
+                    ,'eval' => array('tl_class'=>'w50')
+                )
+
+                // Content
+                ,'slide_content' => array
+                (
+                    'label' => array('Témoignage/citation', 'Saisissez le texte de cet élément')
+                    ,'inputType' => 'textarea'
+                    ,'eval' => array('rte' => 'tinyMCE', 'tl_class' => 'clr')
+                )
+                ,'slide_author' => array
+                (
+                    'label' => array('Auteur', 'Saisissez l\'auteur de cet élément')
+                    ,'inputType' => 'text'
+                    ,'eval' => array('tl_class'=>'w50','tl_class' => 'clr')
+                )
+            )
+        )
+    )
+);
