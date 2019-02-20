de8d2d0e0d5cb496619013bf1a940b93bfed100b
LupusVII <jln.thirion@gmail.com>
2019-02-20 14:46:50 +0100

    Do not apply tl_newsletter changes if package is not present

--- dca/tl_newsletter.php
+++ dca/tl_newsletter.php
@@ -16,20 +16,21 @@
 // unset($GLOBALS['TL_DCA']['tl_newsletter']['config']['sql']['keys']['pid']);
 // unset($GLOBALS['TL_DCA']['tl_newsletter']['fields']['pid']);
 
-$GLOBALS['TL_DCA']['tl_newsletter']['list']['sorting']['mode'] = 1;
-$GLOBALS['TL_DCA']['tl_newsletter']['list']['sorting']['flag'] = 1;
-$GLOBALS['TL_DCA']['tl_newsletter']['list']['label']['fields'] = array('subject');
-$GLOBALS['TL_DCA']['tl_newsletter']['list']['label']['format'] = '%s';
-$GLOBALS['TL_DCA']['tl_newsletter']['list']['label']['label_callback'] = $GLOBALS['TL_DCA']['tl_newsletter']['list']['sorting']['child_record_callback'];
+if (isset($bundles['ContaoNewsletterBundle'])) {
+    $GLOBALS['TL_DCA']['tl_newsletter']['list']['sorting']['mode'] = 1;
+    $GLOBALS['TL_DCA']['tl_newsletter']['list']['sorting']['flag'] = 1;
+    $GLOBALS['TL_DCA']['tl_newsletter']['list']['label']['fields'] = array('subject');
+    $GLOBALS['TL_DCA']['tl_newsletter']['list']['label']['format'] = '%s';
+    $GLOBALS['TL_DCA']['tl_newsletter']['list']['label']['label_callback'] = $GLOBALS['TL_DCA']['tl_newsletter']['list']['sorting']['child_record_callback'];
 
-$GLOBALS['TL_DCA']['tl_newsletter']['palettes']['default'] = str_replace('alias','alias,channels', $GLOBALS['TL_DCA']['tl_newsletter']['palettes']['default']);
+    $GLOBALS['TL_DCA']['tl_newsletter']['palettes']['default'] = str_replace('alias', 'alias,channels', $GLOBALS['TL_DCA']['tl_newsletter']['palettes']['default']);
 
-$GLOBALS['TL_DCA']['tl_newsletter']['fields']['channels'] = array
-(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_newsletter']['channels'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'foreignKey'              => 'tl_newsletter_channel.title',
-	'eval'                    => array('chosen'=>true, 'multiple'=>true, 'tl_class'=>'clr'),
-	'sql'                     => "blob NULL"
-);
+    $GLOBALS['TL_DCA']['tl_newsletter']['fields']['channels'] = array(
+        'label'                   => &$GLOBALS['TL_LANG']['tl_newsletter']['channels'],
+        'exclude'                 => true,
+        'inputType'               => 'checkbox',
+        'foreignKey'              => 'tl_newsletter_channel.title',
+        'eval'                    => array('chosen'=>true, 'multiple'=>true, 'tl_class'=>'clr'),
+        'sql'                     => "blob NULL"
+    );
+}
--- dca/tl_newsletter_channel.php
+++ dca/tl_newsletter_channel.php
@@ -8,9 +8,11 @@
  * @author Web ex Machina <https://www.webexmachina.fr>
  */
 
-unset($GLOBALS['TL_DCA']['tl_newsletter_channel']['config']['ctable'][array_search('tl_newsletter', $GLOBALS['TL_DCA']['tl_newsletter_channel']['config']['ctable'])]);
+if (isset($bundles['ContaoNewsletterBundle'])) {
+    unset($GLOBALS['TL_DCA']['tl_newsletter_channel']['config']['ctable'][array_search('tl_newsletter', $GLOBALS['TL_DCA']['tl_newsletter_channel']['config']['ctable'])]);
 
-$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['edit']['label'] = $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']['label'];
-$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['edit']['href'] = $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']['href'];
-$GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['edit']['button_callback'] = $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']['button_callback'];
-unset($GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']);
+    $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['edit']['label'] = $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']['label'];
+    $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['edit']['href'] = $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']['href'];
+    $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['edit']['button_callback'] = $GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']['button_callback'];
+    unset($GLOBALS['TL_DCA']['tl_newsletter_channel']['list']['operations']['editheader']);
+}
