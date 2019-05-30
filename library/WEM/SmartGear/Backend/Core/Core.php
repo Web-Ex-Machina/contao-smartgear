6ca9d347dae294e9e90d99ea529535db42f150c7
LupusVII <jln.thirion@gmail.com>
2019-05-30 17:03:55 +0200

    Authorize script and iframe tags when installing Smartgear

--- library/WEM/SmartGear/Backend/Core/Core.php
+++ library/WEM/SmartGear/Backend/Core/Core.php
@@ -234,6 +234,7 @@ class Core extends Block implements BlockInterface
             $arrConfig["gdMaxImgWidth"] = 5000;
             $arrConfig["gdMaxImgHeight"] = 5000;
             $arrConfig["maxFileSize"] = 20971520;
+            $arrConfig["allowedTags"] = '<script><iframe><a><abbr><acronym><address><area><article><aside><audio><b><bdi><bdo><big><blockquote><br><base><button><canvas><caption><cite><code><col><colgroup><data><datalist><dataset><dd><del><dfn><div><dl><dt><em><fieldset><figcaption><figure><footer><form><h1><h2><h3><h4><h5><h6><header><hgroup><hr><i><img><input><ins><kbd><keygen><label><legend><li><link><map><mark><menu><nav><object><ol><optgroup><option><output><p><param><picture><pre><q><s><samp><section><select><small><source><span><strong><style><sub><sup><table><tbody><td><textarea><tfoot><th><thead><time><tr><tt><u><ul><var><video><wbr>';
             
             // Update Contao Config
             foreach ($arrConfig as $k => $v) {
