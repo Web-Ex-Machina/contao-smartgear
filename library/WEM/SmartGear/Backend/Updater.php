11c9c88b155934e269552f0c012d9cd60720c3b4
LupusVII <jln.thirion@gmail.com>
2019-11-16 20:07:24 +0100

    Add a function who can find all the update between the current version and the package version

--- library/WEM/SmartGear/Backend/Updater.php
+++ library/WEM/SmartGear/Backend/Updater.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * SMARTGEAR for Contao Open Source CMS
+ * SMARTGEAR for Contao Open Source CMS.
  *
  * Copyright (c) 2015-2019 Web ex Machina
  *
  * @category ContaoBundle
- * @package  Web-Ex-Machina/contao-smartgear
+ *
  * @author   Web ex Machina <contact@webexmachina.fr>
- * @link     https://github.com/Web-Ex-Machina/contao-smartgear/
+ *
+ * @see     https://github.com/Web-Ex-Machina/contao-smartgear/
  */
 
 namespace WEM\SmartGear\Backend;
@@ -23,22 +24,37 @@ use Exception;
 class Updater
 {
     /**
-     * Constructor
+     * Updates to apply.
      *
-     * @param Boolean $shouldBeUpdated [Directly call the update checker]
+     * @var [Array]
      */
-    public function __construct($shouldBeUpdated = true)
+    protected $updates = [];
+
+    /**
+     * Constructor.
+     *
+     * @param bool $checkForUpdates [Directly call the update checker]
+     */
+    public function __construct($checkForUpdates = true)
     {
-        $this->sgVersion = "0.5.0";
+        $this->sgVersion = '0.6.0';
         $this->conf = Util::loadSmartgearConfig();
-        
-        if ($shouldBeUpdated) {
+
+        if ($checkForUpdates) {
             $this->shouldBeUpdated();
         }
     }
 
     /**
-     * Return package Smartgear version
+     * Specific getter.
+     */
+    public function __get($strKey)
+    {
+        return $this->$strKey;
+    }
+
+    /**
+     * Return package Smartgear version.
      *
      * @return [Float] Smartgear version
      */
@@ -48,7 +64,7 @@ class Updater
     }
 
     /**
-     * Return current Smartgear version
+     * Return current Smartgear version.
      *
      * @return [Float] Smartgear version
      */
@@ -58,9 +74,9 @@ class Updater
     }
 
     /**
-     * Play the functions wanted in params
+     * Play the functions wanted in params.
      *
-     * @param String $update Update to call
+     * @param string $update Update to call
      *
      * @return [Boolean] True/False depending of the update status
      */
@@ -69,7 +85,7 @@ class Updater
         try {
             // Check if the update called exists
             if (!method_exists($this, $update)) {
-                throw new \Exception(sprintf("Update %s introuvable !", $update));
+                throw new \Exception(sprintf('Update %s introuvable !', $update));
             }
 
             // Run the update
@@ -84,52 +100,106 @@ class Updater
     }
 
     /**
-     * Generate the update link
+     * Generate the update link.
      *
-     * @param String $update [<description>]
+     * @param string $update [<description>]
      *
-     * @return String - Return the link of the update wanted
+     * @return string - Return the link of the update wanted
      */
     public function getUpdateLink($update)
     {
         try {
-            return sprintf("contao?do=smartgear&sgUpdate=%s", $update);
+            return sprintf('contao?do=smartgear&sgUpdate=%s', $update);
         } catch (\Exception $e) {
             throw $e;
         }
     }
 
     /**
-     * Compare current Smartgear install version with what it should be
-     *
-     * @return void - Return either the function to call either false
+     * Compare current Smartgear install version with what it should be.
      */
     public function shouldBeUpdated()
     {
         try {
             // If no version setup, just call the first
             if (!$this->getCurrentVersion()) {
-                $this->update = "to050";
+                $this->update = 'to050';
             }
 
             // We need to compare current version with the package one
-            $arrPackageVersion = explode(".", $this->getPackageVersion());
-            $arrCurrentVersion = explode(".", $this->getCurrentVersion());
+            $arrPackageVersion = explode('.', $this->getPackageVersion());
+            $arrCurrentVersion = explode('.', $this->getCurrentVersion());
 
             // Check if the system needs an update
             if ($arrCurrentVersion[2] >= $arrPackageVersion[2]
-                || $arrCurrentVersion[1] >= $arrPackageVersion[1]
-                || $arrCurrentVersion[0] >= $arrPackageVersion[0]
+                && $arrCurrentVersion[1] >= $arrPackageVersion[1]
+                && $arrCurrentVersion[0] >= $arrPackageVersion[0]
             ) {
                 $this->update = false;
+
+                return false;
             }
 
             // Now we will find out what is the function to call
             // The point here is not to found the latest update
             // we need to find the closest update to the current version
             // so at each update, the system will detect the next one
-            $strFunction = 'to';
-            // @todo : well, find a way to do this ? :D
+            $x = $arrCurrentVersion[0];
+            $y = $arrCurrentVersion[1];
+
+            // 1st we want to apply all the fix updates of the current major/minor version
+            // We assume that 50 is a good enough limit to the fix version
+            // or we must stop doing whatever we are doing.
+            $maxZ = ($x == $arrPackageVersion[0] && $y == $arrPackageVersion[1]) ? $arrPackageVersion[2] : 50;
+            for ($z = $arrCurrentVersion[2] + 1; $z <= $maxZ; ++$z) {
+                // Format the function name
+                $strFunction = sprintf('to%s%s%s', $x, $y, $z);
+
+                // If it exists, add it to the list of updates to play
+                if (method_exists($this, $strFunction)) {
+                    $this->updates[] = $strFunction;
+                }
+            }
+
+            // Now we have done this, we must redo this, but for minor updates
+            // If we are already on the same major
+            // maxY will be the current package minor
+            // Else, limit that count to 20
+            $maxY = ($x == $arrPackageVersion[0]) ? $arrPackageVersion[1] : 20;
+            for ($y = $arrCurrentVersion[1] + 1; $y <= $maxY; ++$y) {
+                // And check again for fix updates
+                // Here too, check if the current X & Y are equal to the package X & Y
+                $maxZ = ($x == $arrPackageVersion[0] && $y == $arrPackageVersion[1]) ? $arrPackageVersion[2] : 50;
+                for ($z = 0; $z <= $maxZ; ++$z) {
+                    // Format the function name
+                    $strFunction = sprintf('to%s%s%s', $x, $y, $z);
+
+                    // If it exists, add it to the list of updates to play
+                    if (method_exists($this, $strFunction)) {
+                        $this->updates[] = $strFunction;
+                    }
+                }
+            }
+
+            // And finally, do this for major versions
+            // This time, we don't have to go the 50, because we know the last major version for sure
+            for ($x = $arrCurrentVersion[0] + 1; $x <= $arrPackageVersion[0]; ++$x) {
+                // Same for minor
+                for ($y = 0; $y <= $arrPackageVersion[1]; ++$y) {
+                    // Same for fix
+                    for ($z = 0; $z <= $arrPackageVersion[2]; ++$z) {
+                        // Format the function name
+                        $strFunction = sprintf('to%s%s%s', $x, $y, $z);
+
+                        // If it exists, add it to the list of updates to play
+                        if (method_exists($this, $strFunction)) {
+                            $this->updates[] = $strFunction;
+                        }
+                    }
+                }
+            }
+
+            return !empty($this->updates);
         } catch (Exception $e) {
             throw $e;
         }
@@ -137,14 +207,12 @@ class Updater
 
     /**
      * Standardize config to Smartgear 0.5
-     * Just add the version to the current config
-     *
-     * @return void
+     * Just add the version to the current config.
      */
     public function to050()
     {
         try {
-            Util::updateConfig(["sgVersion"=>"0.5.0"]);
+            Util::updateConfig(['sgVersion' => '0.5.0']);
         } catch (Exception $e) {
             throw $e;
         }
