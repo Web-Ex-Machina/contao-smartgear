84acced0d1e43291179f070801feedb1ecfa43c2
ChipsVII <benjamin@webexmachina.fr>
2022-04-20 16:29:31 +0200

    issue #60 : translations for accordion FW

--- src/Resources/public/contao_files/templates/rsce/rsce_accordionFW_config.php
+++ src/Resources/public/contao_files/templates/rsce/rsce_accordionFW_config.php
@@ -13,28 +13,28 @@ declare(strict_types=1);
  */
 
 return [
-    'label' => ['Accordéon (seul)', 'Générez un accordéon et configurez les éléments'], 'contentCategory' => 'accordion', 'standardFields' => ['cssID'], 'fields' => [
+    'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw'], 'contentCategory' => 'accordion', 'standardFields' => ['cssID'], 'fields' => [
         'config_legend' => [
-            'label' => ['Configuration de l\'accordéon'], 'inputType' => 'group',
+            'label' => [&$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['config_legend']], 'inputType' => 'group',
         ], 'deploy_all' => [
-            'label' => ['Tout déployer', 'Cochez pour déployer automatiquement tous les éléments'], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50 clr'],
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['deploy_all'], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50 clr'],
         ], 'disable_collapse' => [
-            'label' => ['Désactiver déployer/replier', 'Cochez pour désactiver les actions déployer/replier (déploie automatiquement tous les éléments)'], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['disable_collapse'], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
         ], 'auto_collapse' => [
-            'label' => ['Auto repliage', "Cochez pour replier automatiquement les éléments lors du déploiement de l'un d'eux"], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['auto_collapse'], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
         ]
 
         // Items
         , 'items' => [
-            'label' => ['Eléments', 'Editez les éléments'], 'elementLabel' => '%s. élément', 'inputType' => 'list', 'fields' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['items_legend'], 'elementLabel' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['item_legend'], 'inputType' => 'list', 'fields' => [
                 // Content
                 'title' => [
                     'label' => &$GLOBALS['TL_LANG']['tl_content']['headline'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'mandatory' => true],
                 ], 'hl_title' => [
-                    'label' => ['Format titre', 'Selectionner un format de titre a appliquer'],
+                    'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['hl_title'],
                     'inputType' => 'select',
                     'options' => [
-                        '' => 'Aucun',
+                        '' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['hl_title']['option_none'],
                         'h1' => 'H1',
                         'h2' => 'H2',
                         'h3' => 'H3',
@@ -44,11 +44,11 @@ return [
                     ],
                     'eval' => ['tl_class' => 'w50 clr', 'mandatory' => false],
                 ], 'content' => [
-                    'label' => ['Contenu', 'Saisissez le contenu textuel de l\'élément'], 'inputType' => 'textarea', 'eval' => ['rte' => 'tinyMCE', 'tl_class' => 'clr', 'mandatory' => true],
+                    'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['content'], 'inputType' => 'textarea', 'eval' => ['rte' => 'tinyMCE', 'tl_class' => 'clr', 'mandatory' => true],
                 ], 'lock' => [
-                    'label' => ['Lock', "Cochez pour que l'élément soit toujours visible (désactive les actions déployer/replier)"], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
+                    'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['lock'], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
                 ], 'active' => [
-                    'label' => ['Active', "Cochez pour que l'élément soit déployé automatiquement au chargement de la page"], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
+                    'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_accordionfw']['active'], 'inputType' => 'checkbox', 'eval' => ['tl_class' => 'w50'],
                 ],
             ],
         ],
