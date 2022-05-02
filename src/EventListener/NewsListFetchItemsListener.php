32f604eb4615fc171f6eda609627191206be3ed4
ChipsVII <93145825+ChipsVII@users.noreply.github.com>
2022-05-02 12:28:59 +0200

    feat : blog filters month (#79)

--- src/EventListener/NewsListFetchItemsListener.php
+++ src/EventListener/NewsListFetchItemsListener.php
@@ -36,10 +36,10 @@ class NewsListFetchItemsListener
             }
 
             if (\array_key_exists('date', $searchConfig) && !empty($searchConfig['date'])) {
-                $date = \DateTime::createFromFormat('Y-m-d', $searchConfig['date']); //format of <input type="date">
+                $date = \DateTime::createFromFormat('Y-m-d', $searchConfig['date']['year'].'-'.$searchConfig['date']['month'].'-01');
                 $col[] = 'date >= ? AND date <= ?';
                 $val[] = $date->setTime(0, 0, 0, 0)->getTimestamp();
-                $val[] = $date->setTime(23, 59, 59)->getTimestamp();
+                $val[] = $date->setTime(0, 0, 0, 0)->add(new \DateInterval('P1M'))->getTimestamp();
             }
 
             return NewsModel::findBy($col, $val, ['limit' => $limit, 'offset' => $offset]);
