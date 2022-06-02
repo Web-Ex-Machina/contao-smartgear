e21e05d5e5980c2c40f6650fe1ffa564ca862507
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-06-02 19:03:22 +0200

    rework admin interface for blockcard

--- src/Resources/public/contao_files/templates/rsce/rsce_blockCard_config.php
+++ src/Resources/public/contao_files/templates/rsce/rsce_blockCard_config.php
@@ -18,34 +18,6 @@ return [
     'contentCategory' => 'SMARTGEAR',
     'standardFields' => ['cssID'],
     'fields' => [
-        'content_order' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['content_order'],
-            'inputType' => 'select',
-            'options' => [
-                'img_first' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['content_order']['optionImg_first'],
-                'txt_first' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['content_order']['optionTxt_first'],
-            ],
-            'eval' => ['tl_class' => 'clr w50'],
-        ],
-        'addRadius' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['addRadius'],
-            'inputType' => 'checkbox',
-            'eval' => ['tl_class' => 'w50'],
-        ],
-        'preset_legend' => [
-            'label' => [&$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset_legend']],
-            'inputType' => 'group',
-        ],
-        'preset' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset'],
-            'inputType' => 'select',
-            'options' => [
-                'light' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset']['optionLight'],
-                'thumbnail' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset']['optionThumbnail'],
-                'inline' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset']['optionInline'],
-            ],
-            'eval' => ['tl_class' => 'w50 clr', 'includeBlankOption' => true],
-        ],
         'image_legend' => [
             'label' => [&$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['image_legend']],
             'inputType' => 'group',
@@ -66,8 +38,66 @@ return [
             'inputType' => 'standardField',
         ],
         'image_css' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['image_css'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50 clr', 'mandatory' => false],
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['image_css'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'mandatory' => false],
         ],
+        'image_displaymode' => array(
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['image_displaymode'],
+            'inputType' => 'select',
+            'options' => array(
+                'img--cover'    => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['image_displaymode']['cover'],
+                'img--contain'  => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['image_displaymode']['contain'],
+                'img--natural'  => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['image_displaymode']['natural'],
+            ),
+            'default' => 'img--cover',
+            'eval' => array('tl_class'=>'w50 clr'),
+        ),
+        'image_ratio' => array(
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['image_ratio'],
+            'inputType' => 'select',
+            'options' => array(
+                '' => 'Original',
+                'r_16-9' => '16:9',
+                'r_4-3'  => '4:3',
+                'r_2-1'  => '2:1',
+                'r_1-1'  => '1:1',
+                'r_1-2'  => '1:2',
+            ),
+            'dependsOn' => array(
+                'field' => 'image_displaymode', 
+                'value' => 'img--cover',
+            ),
+            'eval' => array('tl_class'=>'w50'),
+        ),
+        'image_align_horizontal' => array(
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['image_align_horizontal'],
+            'inputType' => 'select',
+            'options' => array(
+                'left'   => &$GLOBALS['TL_LANG']['tl_content']['alignment']['left'],
+                'center' => &$GLOBALS['TL_LANG']['tl_content']['alignment']['center'],
+                'right'  => &$GLOBALS['TL_LANG']['tl_content']['alignment']['right'],
+            ),
+            'default' =>  'center',
+            'dependsOn' => array(
+                'field' => 'image_displaymode', 
+                'value' => 'img--cover',
+            ),
+            'eval' => array('tl_class'=>'w50 clr'),
+        ),
+        'image_align_vertical' => array(
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['image_align_vertical'],
+            'inputType' => 'select',
+            'options' => array(
+                'top'    => &$GLOBALS['TL_LANG']['tl_content']['alignment']['top'],
+                'center'       => &$GLOBALS['TL_LANG']['tl_content']['alignment']['center'],
+                'bottom' => &$GLOBALS['TL_LANG']['tl_content']['alignment']['bottom'],
+            ),
+            'default' =>  'center',
+            'dependsOn' => array(
+                'field' => 'image_displaymode', 
+                'value' => 'img--cover',
+            ),
+            'eval' => array('tl_class'=>'w50'),
+        ),
         'content_legend' => [
             'label' => [&$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['content_legend']],
             'inputType' => 'group',
@@ -112,6 +142,34 @@ return [
         ],
         'link_css' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['link_css'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50 ', 'mandatory' => false],
+        ],
+        'advanced_legend' => [
+            'label' => [&$GLOBALS['TL_LANG']['tl_content']['advanced_legend']],
+            'inputType' => 'group',
+        ],
+        'content_order' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['content_order'],
+            'inputType' => 'select',
+            'options' => [
+                'img_first' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['content_order']['optionImg_first'],
+                'txt_first' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['content_order']['optionTxt_first'],
+            ],
+            'eval' => ['tl_class' => 'clr w50'],
+        ],
+        'addRadius' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['addRadius'],
+            'inputType' => 'checkbox',
+            'eval' => ['tl_class' => 'clr'],
+        ],
+        'preset' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset'],
+            'inputType' => 'select',
+            'options' => [
+                'light' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset']['optionLight'],
+                'thumbnail' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset']['optionThumbnail'],
+                'inline' => &$GLOBALS['TL_LANG']['tl_content']['rsce_blockcard']['preset']['optionInline'],
+            ],
+            'eval' => ['tl_class' => 'w50 clr', 'includeBlankOption' => true],
         ],
     ],
 ];
