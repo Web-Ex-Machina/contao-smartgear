a25fd715ec4877d1099f679e1a73cc6e85c5113b
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-06-16 15:42:52 +0200

    fix endVal translation reorder fields

--- src/Resources/public/contao_files/templates/rsce/rsce_counter_config.php
+++ src/Resources/public/contao_files/templates/rsce/rsce_counter_config.php
@@ -16,19 +16,26 @@ return [
     'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter'], 'contentCategory' => 'SMARTGEAR', 'standardFields' => ['cssID'], 'fields' => [
         'config_legend' => [
             'label' => [&$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['config_legend']], 'inputType' => 'group',
-        ], 'startVal' => [
+        ], 
+        'startVal' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['startVal'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'mandatory' => true],
-        ], 'endVal' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['startVal'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'mandatory' => true],
-        ], 'decimals' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['decimals'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'minval' => 0],
-        ], 'duration' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['duration'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'minval' => 0],
-        ], 'delay' => [
+        ], 
+        'endVal' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['endVal'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'mandatory' => true],
+        ], 
+        'duration' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['duration'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50 clr', 'rgxp' => 'digit', 'minval' => 0],
+        ], 
+        'delay' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['delay'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'minval' => 0],
-        ], 'unit' => [
+        ], 
+        'decimals' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['decimals'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'minval' => 0],
+        ], 
+        'unit' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['unit'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50'],
-        ], 'label' => [
+        ], 
+        'label' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['label'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50'],
         ],
     ],
