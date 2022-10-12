bbe853ddebdf54276b395580e16265da7a22081e
ChipsVII <benjamin@webexmachina.fr>
2022-10-12 12:28:27 +0200

    Fix : events list shows future events by default

--- src/Override/ModuleEventList.php
+++ src/Override/ModuleEventList.php
@@ -64,13 +64,16 @@ class ModuleEventList extends \Contao\ModuleEventlist
             ],
         ];
 
-        if (null !== Input::get('date')) {
+        if (null !== Input::get('date')
+        && ('' !== Input::get('date')['year'] || '' !== Input::get('date')['month'] || '' !== Input::get('date')['day'])
+        ) {
             $this->config['date']['year'] = Input::get('date')['year'];
             $this->config['date']['month'] = Input::get('date')['month'];
             $this->config['date']['day'] = Input::get('date')['day'];
         } else {
-            $this->config['date']['year'] = date('Y');
-            $this->config['date']['month'] = date('m');
+            $this->cal_format = 'next_all';
+            // $this->config['date']['year'] = date('Y');
+            // $this->config['date']['month'] = date('m');
         }
         $locations = $this->getEventsLocations();
         $this->filters['select']['location'] = [
