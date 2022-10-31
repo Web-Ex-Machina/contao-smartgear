b77d097408217c2c446e6f07ce4db353788915cd
ChipsVII <benjamin@webexmachina.fr>
2022-10-31 10:21:47 +0100

    Feat : do not display breadcrumb if empty

---
+++ src/Override/ModuleBreadcrumb.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SMARTGEAR for Contao Open Source CMS
+ * Copyright (c) 2015-2022 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-smartgear
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-smartgear/
+ */
+
+namespace WEM\SmartgearBundle\Override;
+
+use Contao\System;
+
+class ModuleBreadcrumb extends \Contao\ModuleBreadcrumb
+{
+    public function generate()
+    {
+        $request = System::getContainer()->get('request_stack')->getCurrentRequest();
+
+        if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request)) {
+            return parent::generate();
+        }
+
+        $str = parent::generate();
+
+        if (0 === \count($this->Template->items)) {
+            return '';
+        }
+
+        return $str;
+    }
+}
