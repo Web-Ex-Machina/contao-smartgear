dc82bfbd948334e940a89f378a09be6f069405b5
ChipsVII <benjamin@webexmachina.fr>
2022-12-05 18:00:34 +0100

    Feat : internal analytics (WIP 2)

--- src/Backend/Dashboard/AnalyticsInternal.php
+++ src/Backend/Dashboard/AnalyticsInternal.php
@@ -70,28 +70,75 @@ class AnalyticsInternal extends BackendModule
         // visits this week
         $today = new DateTime('now');
         $arrVisitsThisWeek = [
-            ['date' => (int) $today->getTimestamp() * 1000, 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp() * 1000, 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp() * 1000, 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp() * 1000, 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp() * 1000, 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp() * 1000, 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp() * 1000, 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 7, 'tstamp' => (int) $today->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 6, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 5, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 4, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 3, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 2, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 1, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
         ];
         $this->Template->visitsThisWeek = json_encode(array_reverse($arrVisitsThisWeek));
         // visits last week
         $today = new DateTime('now');
         $today->sub(new DateInterval('P7D'));
         $arrVisitsLastWeek = [
-            ['date' => Date::parse(Config::get('dateFormat'), (int) $today->getTimestamp()), 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => Date::parse(Config::get('dateFormat'), (int) $today->sub(new DateInterval('P1D'))->getTimestamp()), 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => Date::parse(Config::get('dateFormat'), (int) $today->sub(new DateInterval('P1D'))->getTimestamp()), 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => Date::parse(Config::get('dateFormat'), (int) $today->sub(new DateInterval('P1D'))->getTimestamp()), 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => Date::parse(Config::get('dateFormat'), (int) $today->sub(new DateInterval('P1D'))->getTimestamp()), 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => Date::parse(Config::get('dateFormat'), (int) $today->sub(new DateInterval('P1D'))->getTimestamp()), 'value' => $this->getPageVisitsForDay($today)],
-            ['date' => Date::parse(Config::get('dateFormat'), (int) $today->sub(new DateInterval('P1D'))->getTimestamp()), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 7, 'tstamp' => (int) $today->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 6, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 5, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 4, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 3, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 2, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
+            ['index' => 1, 'tstamp' => (int) $today->sub(new DateInterval('P1D'))->getTimestamp(), 'value' => $this->getPageVisitsForDay($today)],
         ];
-        $this->Template->visitsLastWeek = json_encode($arrVisitsLastWeek);
+        $this->Template->visitsLastWeek = json_encode(array_reverse($arrVisitsLastWeek));
+
+        // merged visits
+        $this->Template->visitsMerged = json_encode(array_reverse([
+            [
+                'tstamp' => $arrVisitsThisWeek[0]['tstamp'] * 1000,
+                'dateThisWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsThisWeek[0]['tstamp']),
+                'dateLastWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsLastWeek[0]['tstamp']),
+                'valueThisWeek' => $arrVisitsThisWeek[0]['value'],
+                'valueLastWeek' => $arrVisitsLastWeek[0]['value'],
+            ], [
+                'tstamp' => $arrVisitsThisWeek[1]['tstamp'] * 1000,
+                'dateThisWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsThisWeek[1]['tstamp']),
+                'dateLastWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsLastWeek[1]['tstamp']),
+                'valueThisWeek' => $arrVisitsThisWeek[1]['value'],
+                'valueLastWeek' => $arrVisitsLastWeek[1]['value'],
+            ], [
+                'tstamp' => $arrVisitsThisWeek[2]['tstamp'] * 1000,
+                'dateThisWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsThisWeek[2]['tstamp']),
+                'dateLastWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsLastWeek[2]['tstamp']),
+                'valueThisWeek' => $arrVisitsThisWeek[2]['value'],
+                'valueLastWeek' => $arrVisitsLastWeek[2]['value'],
+            ], [
+                'tstamp' => $arrVisitsThisWeek[3]['tstamp'] * 1000,
+                'dateThisWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsThisWeek[3]['tstamp']),
+                'dateLastWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsLastWeek[3]['tstamp']),
+                'valueThisWeek' => $arrVisitsThisWeek[3]['value'],
+                'valueLastWeek' => $arrVisitsLastWeek[3]['value'],
+            ], [
+                'tstamp' => $arrVisitsThisWeek[4]['tstamp'] * 1000,
+                'dateThisWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsThisWeek[4]['tstamp']),
+                'dateLastWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsLastWeek[4]['tstamp']),
+                'valueThisWeek' => $arrVisitsThisWeek[4]['value'],
+                'valueLastWeek' => $arrVisitsLastWeek[4]['value'],
+            ], [
+                'tstamp' => $arrVisitsThisWeek[5]['tstamp'] * 1000,
+                'dateThisWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsThisWeek[5]['tstamp']),
+                'dateLastWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsLastWeek[5]['tstamp']),
+                'valueThisWeek' => $arrVisitsThisWeek[5]['value'],
+                'valueLastWeek' => $arrVisitsLastWeek[5]['value'],
+            ], [
+                'tstamp' => $arrVisitsThisWeek[6]['tstamp'] * 1000,
+                'dateThisWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsThisWeek[6]['tstamp']),
+                'dateLastWeek' => Date::parse(Config::get('dateFormat'), (int) $arrVisitsLastWeek[6]['tstamp']),
+                'valueThisWeek' => $arrVisitsThisWeek[6]['value'],
+                'valueLastWeek' => $arrVisitsLastWeek[6]['value'],
+            ], ]));
+
         // referers
         // most viewed pages
         $this->Template->modPageUrl = System::getContainer()->get('router')->generate('contao_backend', ['do' => 'page']);
@@ -105,6 +152,6 @@ class AnalyticsInternal extends BackendModule
             'where' => [sprintf('createdAt BETWEEN %d AND %d',
                     $dt->setTime(0, 0, 0, 0)->getTimestamp(),
                     $dt->setTime(23, 59, 59, 999)->getTimestamp()
-                )], ]);
+                )], ]) + 3;
     }
 }
