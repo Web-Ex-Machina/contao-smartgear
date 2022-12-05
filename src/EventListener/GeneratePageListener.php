10868d90e30cf984704ac3678cf6dff5da409fd0
ChipsVII <benjamin@webexmachina.fr>
2022-12-05 15:13:12 +0100

    Feat : page visits statistics

---
+++ src/EventListener/GeneratePageListener.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SMARTGEAR for Contao Open Source CMS
+ * Copyright (c) 2015-2022 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-smartgear
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-smartgear/
+ */
+
+namespace WEM\SmartgearBundle\EventListener;
+
+use Contao\Environment;
+use Contao\LayoutModel;
+use Contao\PageModel;
+use Contao\PageRegular;
+use Contao\System;
+use WEM\SmartgearBundle\Classes\Config\Manager\ManagerJson as CoreConfigurationManager;
+use WEM\SmartgearBundle\Config\Component\Core\Core as CoreConfig;
+use WEM\SmartgearBundle\Exceptions\File\NotFound;
+use WEM\SmartgearBundle\Model\PageVisit;
+
+class GeneratePageListener
+{
+    /** @var CoreConfigurationManager */
+    protected $configurationManager;
+
+    public function __construct(
+        CoreConfigurationManager $configurationManager
+    ) {
+        $this->configurationManager = $configurationManager;
+    }
+
+    public function __invoke(PageModel $pageModel, LayoutModel $layout, PageRegular $pageRegular): void
+    {
+        try {
+            /** @var CoreConfig */
+            $config = $this->configurationManager->load();
+        } catch (NotFound $e) {
+            return;
+        }
+
+        if (\defined('TL_MODE') && TL_MODE !== 'FE') {
+            return;
+        }
+
+        // add a new visit
+        $objItem = new PageVisit();
+        $objItem->pid = $pageModel->id;
+        $objItem->page_url = Environment::get('uri');
+        $objItem->referer = System::getReferer();
+        $objItem->createdAt = time();
+        $objItem->tstamp = time();
+        $objItem->save();
+    }
+}
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -183,6 +183,7 @@ if ('FE' === TL_MODE) {
     $GLOBALS['TL_HOOKS']['createNewUser'][] = ['smartgear.listener.create_new_user', '__invoke'];
     $GLOBALS['TL_HOOKS']['processFormData'][] = ['smartgear.listener.process_form_data', '__invoke'];
     $GLOBALS['TL_HOOKS']['compileFormFields'][] = ['smartgear.listener.compile_form_fields', '__invoke'];
+    $GLOBALS['TL_HOOKS']['generatePage'][] = ['smartgear.listener.generate_page', '__invoke'];
 }
 
 /*
