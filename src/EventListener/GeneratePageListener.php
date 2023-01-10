9dc161a0ad44262007ceebf7cc925cf23a009d87
ChipsVII <benjamin@webexmachina.fr>
2023-01-10 18:10:43 +0100

    Fix : user id hash sometimes empty

--- src/EventListener/GeneratePageListener.php
+++ src/EventListener/GeneratePageListener.php
@@ -101,8 +101,10 @@ class GeneratePageListener
             return;
         }
 
-        if (null === Util::getCookieVisitorUniqIdHash()) {
-            Util::setCookieVisitorUniqIdHash();
+        $hash = Util::getCookieVisitorUniqIdHash();
+        if (null === $hash) {
+            $hash = Util::buildCookieVisitorUniqIdHash();
+            Util::setCookieVisitorUniqIdHash($hash);
         }
 
         // add a new visit
@@ -111,7 +113,7 @@ class GeneratePageListener
         $objItem->page_url = Environment::get('uri');
         $objItem->referer = System::getReferer();
         $objItem->url = System::getReferer();
-        $objItem->hash = Util::getCookieVisitorUniqIdHash();
+        $objItem->hash = $hash;
         $objItem->createdAt = time();
         $objItem->tstamp = time();
         $objItem->save();
