e933466b26d7caa040b3bf7fb9a5dffb927a7ed8
ChipsVII <benjamin@webexmachina.fr>
2023-03-31 22:05:26 +0200

    Fix : only use FindOneById for FormField model

--- src/Backend/Component/FormContact/Resetter.php
+++ src/Backend/Component/FormContact/Resetter.php
@@ -136,27 +136,27 @@ class Resetter extends BackendResetter
                     $objFormContact->delete();
                 }
 
-                $objField = FormFieldModel::findByIdOrAlias($formContactConfig->getSgFieldName());
+                $objField = FormFieldModel::findOneById($formContactConfig->getSgFieldName());
                 if ($objField) {
                     $objField->delete();
                 }
 
-                $objField = FormFieldModel::findByIdOrAlias($formContactConfig->getSgFieldEmail());
+                $objField = FormFieldModel::findOneById($formContactConfig->getSgFieldEmail());
                 if ($objField) {
                     $objField->delete();
                 }
 
-                $objField = FormFieldModel::findByIdOrAlias($formContactConfig->getSgFieldMessage());
+                $objField = FormFieldModel::findOneById($formContactConfig->getSgFieldMessage());
                 if ($objField) {
                     $objField->delete();
                 }
 
-                $objField = FormFieldModel::findByIdOrAlias($formContactConfig->getSgFieldCaptcha());
+                $objField = FormFieldModel::findOneById($formContactConfig->getSgFieldCaptcha());
                 if ($objField) {
                     $objField->delete();
                 }
 
-                $objField = FormFieldModel::findByIdOrAlias($formContactConfig->getSgFieldSubmit());
+                $objField = FormFieldModel::findOneById($formContactConfig->getSgFieldSubmit());
                 if ($objField) {
                     $objField->delete();
                 }
