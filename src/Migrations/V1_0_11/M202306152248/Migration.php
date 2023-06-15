f10960805f0a45e0470c21f56fd3925afa64cc51
ChipsVII <benjamin@webexmachina.fr>
2023-06-15 22:52:36 +0200

    Feat : v1.0.11

---
+++ src/Migrations/V1_0_11/M202306152248/Migration.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SMARTGEAR for Contao Open Source CMS
+ * Copyright (c) 2015-2022 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-smartgear
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-smartgear/
+ */
+
+namespace WEM\SmartgearBundle\Migrations\V1_0_11\M202306152248;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use WEM\SmartgearBundle\Classes\Config\Manager\ManagerJson as CoreConfigurationManager;
+use WEM\SmartgearBundle\Classes\Migration\Result;
+use WEM\SmartgearBundle\Classes\Version\Comparator as VersionComparator;
+use WEM\SmartgearBundle\Config\Component\Core\Core as CoreConfig;
+use WEM\SmartgearBundle\Migrations\V1_0_0\MigrationAbstract;
+
+class Migration extends MigrationAbstract
+{
+    protected static $name = 'Smargear update to v1.0.11';
+    protected static $description = 'Set Smartgear to version 1.0.11';
+    protected static $version = '1.0.11';
+    protected static $translation_key = 'WEMSG.MIGRATIONS.V1_0_10_M202306152248';
+
+    public function __construct(
+        Connection $connection,
+        TranslatorInterface $translator,
+        CoreConfigurationManager $coreConfigurationManager,
+        VersionComparator $versionComparator
+    ) {
+        parent::__construct($connection, $translator, $coreConfigurationManager, $versionComparator);
+    }
+
+    public function shouldRun(): Result
+    {
+        $result = parent::shouldRun();
+
+        if (Result::STATUS_SHOULD_RUN !== $result->getStatus()) {
+            return $result;
+        }
+
+        $result
+            ->addLog($this->translator->trans('WEMSG.MIGRATIONS.shouldBeRun', [], 'contao_default'))
+        ;
+
+        return $result;
+    }
+
+    public function do(): Result
+    {
+        $result = $this->shouldRun();
+        if (Result::STATUS_SHOULD_RUN !== $result->getStatus()) {
+            return $result;
+        }
+        try {
+            /** @var CoreConfig */
+            $coreConfig = $this->coreConfigurationManager->load();
+
+            $coreConfig->setSgVersion(self::$version);
+
+            $this->coreConfigurationManager->save($coreConfig);
+
+            $result
+                ->setStatus(Result::STATUS_SUCCESS)
+                ->addLog($this->translator->trans($this->buildTranslationKey('done'), [], 'contao_default'))
+            ;
+        } catch (\Exception $e) {
+            $result
+                ->setStatus(Result::STATUS_FAIL)
+                ->addLog($e->getMessage())
+            ;
+        }
+
+        return $result;
+    }
+}
