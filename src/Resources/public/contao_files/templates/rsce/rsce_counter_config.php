86c0e101d0d94fba792390f7d23d4042ddf4868b
SeptimusVII <vansteene.quentin@gmail.com>
2023-07-19 08:29:01 +0200

    Feat: improve rsce counter (#270)
    - add fields prefix, decimal, separator, icon and color
    - reorganize backend fields placement
    - add translations for new fields
    - adapt template to new fields

--- src/Resources/public/contao_files/templates/rsce/rsce_counter_config.php
+++ src/Resources/public/contao_files/templates/rsce/rsce_counter_config.php
@@ -22,21 +22,48 @@ return [
         ], 
         'endVal' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['endVal'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'mandatory' => true],
+        ],
+        'prefix' =>[
+            'label' =>&$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['prefix'],
+            'inputType' => 'text',
+            'eval' =>['tl_class' => 'w50']
+        ],
+        'unit' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['unit'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50'],
         ], 
+        'decimals' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['decimals'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'minval' => 0],
+        ], 
+        'decimal' =>[
+            'label' =>&$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['decimal'],
+            'inputType' => 'text',
+            'eval' =>['tl_class' => 'w50']
+        ],
+        'separator' =>[
+            'label' =>&$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['separator'],
+            'inputType' => 'checkbox',
+            'default' => 1,
+            'eval' =>['tl_class' => 'w50 clr']
+        ],
         'duration' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['duration'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50 clr', 'rgxp' => 'digit', 'minval' => 0],
         ], 
         'delay' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['delay'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'minval' => 0],
         ], 
-        'decimals' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['decimals'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50', 'rgxp' => 'digit', 'minval' => 0],
-        ], 
-        'unit' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['unit'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50'],
-        ], 
         'label' => [
             'label' => &$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['label'], 'inputType' => 'text', 'eval' => ['tl_class' => 'w50'],
+        ],
+        'icon' =>[
+            'label' =>&$GLOBALS['TL_LANG']['tl_content']['rsce_counter']['icon'],
+            'inputType' => 'text',
+            'eval' =>['tl_class' => 'w50','allowHtml'=>true]
+        ],
+        'color' =>[
+            'label' =>&$GLOBALS['TL_LANG']['tl_content']['dominant_color'],
+            'inputType' => 'select',
+            'options' => \WEM\SmartgearBundle\Classes\Util::getSmartgearColors(),
+            'eval' =>['tl_class'=>'w50','includeBlankOption'=>true]
         ],
     ],
 ];
