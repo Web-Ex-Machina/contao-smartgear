c37697bf0935b03c11b0564540a8143a4458163b
ChipsVII <benjamin@webexmachina.fr>
2023-08-08 09:16:27 +0200

    Feat : non expert users have to select an image size

--- src/Backend/Component/Core/EventListener/LoadDataContainerListener.php
+++ src/Backend/Component/Core/EventListener/LoadDataContainerListener.php
@@ -57,7 +57,8 @@ class LoadDataContainerListener
                         // do not display grid_gap settings
                         $this->dcaManipulator->removeFields(['grid_gap']);
                         $this->dcaManipulator->setFieldEvalProperty('cssID', 'tl_class', 'hidden');
-                        $this->dcaManipulator->setFieldEvalProperty('size', 'includeBlankOption', 'false');
+                        // $this->dcaManipulator->setFieldEvalProperty('size', 'includeBlankOption', 'false');
+                        unset($GLOBALS['TL_DCA']['tl_content']['fields']['size']['eval']['includeBlankOption']);
                         $this->dcaManipulator->setFieldEvalProperty('size', 'mandatory', 'true');
                         $this->updatePaletteHeadline();
                         $this->updatePaletteText();
