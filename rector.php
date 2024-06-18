a89d2307a015d9c9a40049c90d5f259e8c9fb145
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-06-18 14:42:50 +0200

    feat: add DeadCodeLevel and skip rule in rector config

--- rector.php
+++ rector.php
@@ -2,11 +2,16 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([__DIR__ . '/src'])
     ->withPhpSets(php83: true)
+    ->withDeadCodeLevel(level:1)
+    ->withSkip([
+        CallableThisArrayToAnonymousFunctionRector::class
+    ])
     ->withAttributesSets(symfony: true, doctrine: true)
-    ->withPreparedSets(deadCode: true, codeQuality: true, codingStyle: true, typeDeclarations: true)
+    ->withPreparedSets(codeQuality: true, codingStyle: true, typeDeclarations: true)
     ;
