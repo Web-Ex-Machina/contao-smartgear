157aea75626038df98683e235a42346c7c8c27cf
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-06-18 14:32:44 +0200

    refactor: Ensure match parameters are arrays in callbacks

--- src/Widget/ComponentStyleSelect.php
+++ src/Widget/ComponentStyleSelect.php
@@ -24,7 +24,7 @@ class ComponentStyleSelect extends \Oveleon\ContaoComponentStyleManager\Widget\C
         // normal translation keys
         $content = preg_replace_callback(
             '/>([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)</',
-            static function ($match) : string {
+            static function (array $match) : string {
                 $translation = sprintf('>%s<', $GLOBALS['TL_LANG'][$match[1]][$match[2]][$match[3]][$match[4]]);
                 return $translation ?: implode('.', $match);
             },
@@ -33,7 +33,7 @@ class ComponentStyleSelect extends \Oveleon\ContaoComponentStyleManager\Widget\C
         // combined translation keys (with color)
         $content = preg_replace_callback(
             '/>([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+) \(([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\)</',
-            static function ($match) : string {
+            static function (array $match) : string {
                 $colorTranslation = $GLOBALS['TL_LANG'][$match[5]][$match[6]][$match[7]][$match[8]] ?? $match[8];
                 return sprintf('>%s<', sprintf($GLOBALS['TL_LANG'][$match[1]][$match[2]][$match[3]][$match[4]], $colorTranslation));
             },
@@ -43,9 +43,7 @@ class ComponentStyleSelect extends \Oveleon\ContaoComponentStyleManager\Widget\C
         // normal translation keys for optgroup
         $content = preg_replace_callback(
             '/label\="([\s|&nbsp;]+)([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)\.([A-Za-z0-9\_\-]+)"/',
-            static function ($match) : string {
-                return sprintf('label="%s%s"', $match[1], $GLOBALS['TL_LANG'][$match[2]][$match[3]][$match[4]][$match[5]]);
-            },
+            static fn($match): string => sprintf('label="%s%s"', $match[1], $GLOBALS['TL_LANG'][$match[2]][$match[3]][$match[4]][$match[5]]),
             $content
         );
 
@@ -55,8 +53,8 @@ class ComponentStyleSelect extends \Oveleon\ContaoComponentStyleManager\Widget\C
                 // $match[0] => full match
 
                 // look in the options if we have something color related ?
-                if (preg_match('|<option value="(.*)-primary"|', (string) $match[2])
-                || preg_match('|<option value="(.*)-red"|', (string) $match[2])
+                if (preg_match('|<option value="(.*)-primary"|', $match[2])
+                || preg_match('|<option value="(.*)-red"|', $match[2])
                 ) {
                     $objConfiguration = ConfigurationUtil::findConfigurationForItem($this->arrConfiguration['strTable'], (int) $this->arrConfiguration['currentRecord']);
                     $helpA = '<a href="contao/help.php?table='.$this->arrConfiguration['strTable'].'&amp;field='.$this->arrConfiguration['strField'].'&amp;id='.$this->arrConfiguration['currentRecord'].'&amp;framway_path='.($objConfiguration instanceof \WEM\SmartgearBundle\Model\Configuration\Configuration ? $objConfiguration->framway_path : \WEM\SmartgearBundle\Model\Configuration\Configuration::DEFAULT_FRAMWAY_PATH).'" title="'.\Contao\StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['helpWizard']).'" onclick="Backend.openModalIframe({\'title\':\''.\Contao\StringUtil::specialchars($GLOBALS['TL_LANG']['WEMSG']['FRAMWAY']['COLORS']['helpWizardTitle']).'\',\'url\':this.href});return false">'.\Contao\Image::getHtml('about.svg', $GLOBALS['TL_LANG']['MSC']['helpWizard']).'</a>';
