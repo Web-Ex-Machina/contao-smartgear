2ca897d0b93c313213bbe4a8f2ae26b99f6ac895
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-07-02 14:10:04 +0200

    update ModuleLogin for use UrlGeneratorInterface

--- src/Override/ModuleLogin.php
+++ src/Override/ModuleLogin.php
@@ -15,9 +15,12 @@ declare(strict_types=1);
 namespace WEM\SmartgearBundle\Override;
 
 use Contao\PageModel;
+use Contao\System;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ModuleLogin extends \Contao\ModuleLogin
 {
+
     public function generate(): string
     {
         return parent::generate();
@@ -25,19 +28,24 @@ class ModuleLogin extends \Contao\ModuleLogin
 
     /**
      * Generate the module.
+     * @throws \Exception
      */
     protected function compile(): void
     {
+
         parent::compile();
+
+        /* @var UrlGeneratorInterface $routeGenerator*/
+        $routeGenerator = System::getContainer()->get('contao.routing.content_url_generator');
 
         if (($objTarget = $this->objModel->getRelated('wem_sg_login_pwd_lost_jumpTo')) instanceof PageModel) {
             $this->Template->wem_sg_login_pwd_lost_jumpTo_label = $GLOBALS['TL_LANG']['WEM']['SMARTGEAR']['DEFAULT']['passwordLost'];
-            $this->Template->wem_sg_login_pwd_lost_jumpTo = $objTarget->getAbsoluteUrl(); //TODO : DEPRECATED
+            $this->Template->wem_sg_login_pwd_lost_jumpTo = $routeGenerator->generate($objTarget->name, [], UrlGeneratorInterface::ABSOLUTE_URL);
         }
 
         if (($objTarget = $this->objModel->getRelated('wem_sg_login_register_jumpTo')) instanceof PageModel) {
             $this->Template->wem_sg_login_register_jumpTo_label = $GLOBALS['TL_LANG']['WEM']['SMARTGEAR']['DEFAULT']['register'];
-            $this->Template->wem_sg_login_register_jumpTo = $objTarget->getAbsoluteUrl(); //TODO : DEPRECATED
+            $this->Template->wem_sg_login_register_jumpTo = $routeGenerator->generate($objTarget->name, [], UrlGeneratorInterface::ABSOLUTE_URL);
         }
     }
 }
