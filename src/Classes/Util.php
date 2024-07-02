da85f4ef0edc67b58680a08e0a16f8ccad42cb29
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2024-07-02 15:40:22 +0200

    Remove useless commentary

--- src/Classes/Util.php
+++ src/Classes/Util.php
@@ -553,8 +553,7 @@ class Util
 
     public static function humanReadableFilesize(int $size, ?int $precision = 2): string
     {
-        for ($i = 0; ($size / 1024) > 0.9; $i++, $size /= 1024) {
-        } //TODO : Useless ??
+        for ($i = 0; ($size / 1024) > 0.9; $i++, $size /= 1024) {}
 
         return round($size, $precision).['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][$i];
     }
