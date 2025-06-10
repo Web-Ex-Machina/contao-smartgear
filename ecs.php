0faff71261f1e981a9c7da0fe6665f414e4a9c7b
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2025-06-10 15:36:33 +0200

    ecs config

--- ecs.php
+++ ecs.php
@@ -2,35 +2,58 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
-use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
-use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
+/*
+ * SMARTGEAR for Contao Open Source CMS
+ * Copyright (c) 2015-2025 Web ex Machina
+ *
+ * @category ContaoBundle
+ * @package  Web-Ex-Machina/contao-smartgear
+ * @author   Web ex Machina <contact@webexmachina.fr>
+ * @link     https://github.com/Web-Ex-Machina/contao-smartgear/
+ */
 
+use Contao\EasyCodingStandard\Fixer\CommentLengthFixer;
+use Contao\EasyCodingStandard\Fixer\FunctionCallWithMultilineArrayFixer;
+use Contao\EasyCodingStandard\Fixer\MultiLineIfIndentationFixer;
+use Contao\EasyCodingStandard\Fixer\NoLineBreakBetweenMethodArgumentsFixer;
+use Contao\EasyCodingStandard\Fixer\TypeHintOrderFixer;
 use Contao\EasyCodingStandard\Set\SetList;
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
+use PhpCsFixer\Fixer\Import\FullyQualifiedStrictTypesFixer;
+use PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return ECSConfig::configure()->withSets([SetList::CONTAO])
+return ECSConfig::configure()
+    ->withSets([SetList::CONTAO])
     ->withConfiguredRule(HeaderCommentFixer::class, configuration: [
-        'header' =>
-"SMARTGEAR for Contao Open Source CMS
-Copyright (c) 2015-".date("Y")." Web ex Machina
+        'header' => 'SMARTGEAR for Contao Open Source CMS
+Copyright (c) 2015-' . date('Y') . ' Web ex Machina
 
 @category ContaoBundle
 @package  Web-Ex-Machina/contao-smartgear
 @author   Web ex Machina <contact@webexmachina.fr>
-@link     https://github.com/Web-Ex-Machina/contao-smartgear/"])
-    ->withPaths([
-    __DIR__ . '/src',
-])
-->withSkip([
-    ArrayOpenerAndCloserNewlineFixer::class,
-    ArraySyntaxFixer::class,
-    __DIR__ . '/src/Migrations',
-])
-
-    // NOTE : common intègre les règles : arrays, spaces, namespaces, docblocks, controlStructures, phpunit, comments
-->withPreparedSets(
-    $psr12=true,
-    $common=true,
-    $strict=true,
-)->withRootFiles();
+@link     https://github.com/Web-Ex-Machina/contao-smartgear/',
+    ])
+    ->withPaths([__DIR__ . '/src'])
+    ->withSkip([
+        __DIR__ . '/src/Migrations',
+        TypeHintOrderFixer::class,
+        NoLineBreakBetweenMethodArgumentsFixer::class,
+        GlobalNamespaceImportFixer::class,
+        FullyQualifiedStrictTypesFixer::class,
+        NativeFunctionInvocationFixer::class,
+        MultiLineIfIndentationFixer::class,
+        FunctionCallWithMultilineArrayFixer::class,
+        CommentLengthFixer::class
+    ])
+    ->withPreparedSets(
+        psr12: true,
+        comments: true,
+        docblocks: true,
+        spaces: true,
+        namespaces: true,
+        controlStructures: true,
+        strict: true,
+        cleanCode: true,
+    );
